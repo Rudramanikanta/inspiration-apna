4203468df25a63e1ad6bf58db42adfbecc0ee9ab
Your NameRudramanikanta <rudramanikanta02@gmail.com>
2023-03-22 10:46:23 +0530

    "commit third"

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 var buttons = document.querySelectorAll("#fury")
 console.log(buttons)
 buttons.forEach((value) => {
     value.addEventListener("click", (event) => {
-        console.log(event.target.value)
+
         window.open(`https://www.youtube.com/results?search_query=${event.target.value}+trailer`, "", "width=200px;heigth=200")
     })
 })
@@ -10,7 +11,28 @@ console.log(navigator)
 var divs = document.querySelectorAll(".card")
 divs.forEach((value) => {
     value.addEventListener("mouseover", (event) => {
-        
-        
+
+
     })
 })
+setTimeout(() => {
+    document.getElementById("spinner2").style.display = "none"
+}, 2000)
+
+var search = document.getElementById("search")
+var cards = document.querySelectorAll(".card")
+search.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+
+      cards.forEach((value)=>{
+       if(value.attributes.name.nodeValue===search.value){
+            display(value)
+       }
+      })
+      
+    }
+})
+function display(data){
+    document.getElementById("body").innerHTML=""
+    document.getElementById("body").appendChild(data)
+}
